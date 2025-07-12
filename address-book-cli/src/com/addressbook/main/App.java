f66b06d13334c2d69c2ce85e47faec1c868e3ebb
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-12 21:37:25 +0545

    Add Menu and Contact class

--- address-book-cli/src/com/addressbook/main/App.java
+++ address-book-cli/src/com/addressbook/main/App.java
@@ -1,4 +1,86 @@
 package com.addressbook.main;
+
+import java.util.Scanner;
+
+// import com.addressbook.service.Service;
+// import com.addressbook.util.Utility;
+
 public class App {
-    
+    public static void main(String[] args) {
+        // Service serv = new Service();
+        // Utility util = new Utility();
+
+        Scanner sc = new Scanner(System.in);
+        
+        boolean isExit = false;
+        System.out.print("Loading state ");
+
+        while(!isExit) {
+            try {
+                Thread.sleep(500);
+                System.out.print(".");
+                Thread.sleep(500);
+                System.out.print(".");
+                Thread.sleep(500);
+                System.out.println(".");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n");
+
+            System.out.println("=== Address Book CLI ===\n");
+            System.out.println("1) Add Contact\n2) List All Contacts\n3) Search Contacts\n4) Edit Contact\n5) Delete Contact\n6) Import vCard (.vcf)\n7) Export Contact to vCard\n8) Undo Last Action\n" + //
+                                "9) Redo Last Action\n0) Exit");
+            System.out.print("Enter Choice: ");
+            int choice = sc.nextInt();
+            switch(choice) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 0:
+                    isExit = true;
+                    break;
+                default:
+                    System.out.println("Invalid input!!");
+            }
+        }
+        System.out.print("Saving state and Exiting ");
+        try {
+            Thread.sleep(500);
+            System.out.print(".");
+            Thread.sleep(500);
+            System.out.print(".");
+            Thread.sleep(500);
+            System.out.println(".");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        sc.close();
+    }
 }
--- address-book-cli/src/com/addressbook/model/Contact.java
+++ address-book-cli/src/com/addressbook/model/Contact.java
@@ -1,4 +1,38 @@
 package com.addressbook.model;
 public class Contact {
-    
+    private String firstname;
+    private String lastname;
+    private long mobile;
+    private String address;
+    private String email;
+    private String github;
+
+    @Override
+	public String toString() {
+        return "Person [firstname=" + firstname + ", lastname=" + lastname + ", address=" + address + ", mobile=" + mobile + ", email=" + email + ", github=" + github + "]";
+    }
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
 }
