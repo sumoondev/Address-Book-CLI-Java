36c315e2f38cc08a3a9011df2d06655ea1f15405
Sumoon Byanjankar <sumoon.dev@gmail.com>
2025-07-15 22:57:15 +0545

    Add readJson()

--- address-book-cli/src/com/addressbook/util/Utility.java
+++ address-book-cli/src/com/addressbook/util/Utility.java
@@ -1,5 +1,42 @@
 package com.addressbook.util;
 
+import java.io.File;
+import java.io.IOException;
+
+// import com.fasterxml.jackson.databind.ObjectMapper;
+import com.addressbook.model.Contact;
+
 public class Utility {
-    
+    private String path = "address-book-cli/data/addressbook.json";
+    public boolean readJson(Contact persons[]) {
+        try {
+            File file = new File(path);
+            if(file.exists()) {
+                if(file.length() != 0) {
+                    // ObjectMapper objectmapper = new ObjectMapper();
+
+                    System.out.println("Contact(s) found !!");
+                    Thread.sleep(1000);
+                    return true;
+                }
+                System.out.println("Contact found 0");
+                for(int i = 0; i < persons.length; i++) {
+                    persons[i] = null;
+                }
+                Thread.sleep(1000);
+                return true;
+            }
+            System.out.println("No file found to read!!");
+            file.createNewFile();
+            Thread.sleep(1000);
+            return true;
+        } catch (IOException e) {
+            System.out.println("An error occurred.!!!");
+            e.printStackTrace();
+        } catch (InterruptedException f) {
+            Thread.currentThread().interrupt();
+        }
+        
+        return false;
+    }
 }
